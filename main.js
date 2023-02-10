b2a0baa0441936c8aadfa9815e9a2dd82ab70e6e
Renata Novais <98842524+rejuno@users.noreply.github.com>
2023-02-10 14:36:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-
 const form = document.getElementById('form');
 
-/*function funcao(campoA,campoB){
-    if(campoA > campoB){
-        alert("O Campo B deve ser maior que o Campo A");
-    } else{
-        alert("Enviado!")
-    }
-}*/
+
 form.addEventListener('submit', function(funcao){
     const campoA = document.getElementById('campoA');
     const campoB = document.getElementById('campoB');
